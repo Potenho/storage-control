9755b83e4ffe5936d25e90c3dfc4ad09d266b739
Potenho <viisael97@hotmail.com>
2024-11-24 22:31:20 -0300

    Co-authored-by: renatocezario03@gmail.com <renatocezario03@gmail.com>: Improve main page

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { NavMenuComponent } from '../../design-system/nav-menu/nav-menu.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, NavMenuComponent],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
