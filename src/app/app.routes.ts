799b5e71b2bd758ce06c5f448d8966c1f4b8dbe7
Potenho <viisael97@hotmail.com>
2024-11-27 21:15:13 -0300

    :construction: WIP (Register Modal): Implementing modal
    Co-authored-by: renatocezario03@gmail.com <renatocezario03@gmail.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
 import { LoginComponent } from './modules/login/login.component';
 import { DefaultComponent } from './modules/default/default.component';
+import { RegisterComponent } from './modules/register/register.component';
 
 export const routes: Routes = [
     {
@@ -20,6 +21,10 @@ export const routes: Routes = [
             {
                 path: 'login',
                 component: LoginComponent
+            },
+            {
+                path: 'register',
+                component: RegisterComponent
             }
         ]
     },
